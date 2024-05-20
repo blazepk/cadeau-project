d549d94465bc9d48c2eb45e8e3fcca89f2712387
Mahak Khandelwal <khandelwal.mahak311@gmail.com>
2024-05-20 12:54:04 +0200

    iniate

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,9 +3,9 @@
 
 function Contact() {
   return (
-    <div className="py-16 bg-white">
-          <div className="flex w-full min-h-screen justify-center items-center">
-              <div className= 'flex- flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-cyan-700 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white'>
+    
+          <div className="grid gap-4 m-4 sm:grid-cols-12 bg-white">
+              <div className= 'min-h-[100px] rounded  bg-yellow-700 w-full shadow-xl text-white sm:col-span-4'>
                   < div className='flex flex-col space-y-8 justify-between'>
                         <div >
                             <h1 className='font-bold text-4xl tracking-wide'>Contact Us</h1>
@@ -28,35 +28,35 @@ function Contact() {
                         </div>
                         
                   </div>
-
-                  <div className='bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80'>
+                  
+                  <div className='bg-white min-h-[100px] rounded w-full shadow-xl md:col-span-8 text-gray-600'>
                     <form action='' className='flex flex-cols space-y-4'>
                       <div>
                         <label className='text-sm'>Your Name</label> 
                           <input type='text' 
                           placeholder='your Name' 
-                          className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300'/>
+                          className='min-h-[50px] ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300'/>
                       </div>
                       <div>
                         <label className='text-sm'>Email Address</label> 
                           <input type='email' 
                           placeholder='Your Email' 
-                          className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300'/>
+                          className='ring-1 ring-gray-300 w-full rounded-md px-4 py-4 mt-2 outline-none focus:ring-2 focus:ring-teal-300'/>
                       </div>
                       <div>
                         <label className='text-sm'>Message</label> 
                           <textarea
                           placeholder='Message' 
-                          rows='4'
+                          rows= {4}
                           className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300'></textarea>
                       </div>
 
-                      <button className='inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase-sm'>Send Message</button>
+                      <button className='inline-block self-end bg-yellow-700 text-white font-bold rounded-lg px-6 py-2 uppercase-sm'>Send Message</button>
                     </form>
                   </div>
               </div>
             </div>
-          </div>     
+             
                   
 
                       
--- src/components/navbar/FlowbiteNav.tsx
+++ src/components/navbar/FlowbiteNav.tsx
@@ -19,25 +19,7 @@ function NavComponent() {
           </span>
         </Link>
         < div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
-                        <div className='flex justify-between items-center pt-8'>
-                            <div className='relative w-full max-w-[500px]'>
-                            <input
-                            
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                            type='text'
-                            placeholder='Search Product' />
-                            <BsSearch
-                                className='absolute top-0 right-0 mt-4 mr-5 text-grey-500'
-                                size={20}
-                            />
-                        </div>
-                    </div>
+                        
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -95,13 +77,7 @@ function NavComponent() {
                 Services
               </NavLink>
             </li>
-            <li>
-              <NavLink to ="/Pricing"
-                className= {({isActive}) => `block py-2 px-3 text-gray-900 rounded ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-              >
-                Pricing
-              </NavLink>
-            </li>
+            
             <li>
               <NavLink to ="/Contact"
                 className= {({isActive})=> `block py-2 px-3 text-gray-900 rounded ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
