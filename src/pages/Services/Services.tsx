7fd8fcec1f2d3586ebe6d4f52e03092ca87074c5
Mahak Khandelwal <khandelwal.mahak311@gmail.com>
2024-06-04 12:22:43 +0200

    contactform

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,96 +1,81 @@
-import React from 'react'
+const serviceCardDetails = [
+  {
+    id: 1,
+    title: "Exquisite Presentation",
+    description: `Our gifts aren't just products, they are elegantly packaged to leave
+            a lasting impression, show-casing your attention to detail and
+            appreciation for the recipient`,
+  },
+  {
+    id: 2,
+    title: "Unmatched Customer Support",
+    description: `Our responsive customer support team is always at your service
+                ensuring a smooth and stress-free gifting experience`,
+  },
+  {
+    id: 3,
+    title: "Sustainability Commitment",
+    description: `We're dedicated to eco-conscious gifting. Our catalog features a
+                range of eco - friendly products , aligning your brandwith
+                sustainability goals`,
+  },
+  {
+    id: 4,
+    title: "Cost-Efficiency Beyond MRP",
+    description: `We prioritize affordability.Our exclusive partnerships enable us
+                to offer you corporate gifts at prices lower than their MRP,
+                ensuring exceptional value for your investment`,
+  },
+  {
+    id: 5,
+    title: "Effortless Corporate Gifting",
+    description: `Simplify your gifting process with our themed gift sets and
+                expert curation, making it effortless for you to select the
+                perfect gifts for your clients, employees, and partners.`,
+  },
+  {
+    id: 6,
+    title: "Diverse Curated Selection",
+    description: `We excel in meticulously curating top-tie brands across a wide
+                spectrum of categories ensuring that our corporate gifts are a
+                fusion of innovation, quality, and purpose`,
+  },
+];
+
+interface IServiceData {
+  id: number;
+  title: string;
+  description: string;
+}
+
+function ServiceCard({ title, description }: IServiceData) {
+  return (
+    <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl p-4 text-white  col-span-6">
+      <div className="p-4 text-center flex flex-col justify-around h-full">
+        <div className="flex flex-col gap-y-2">
+          <h2 className="text-l text-teal-900 font-bold md:text-xl">{title}</h2>
+          <p className="mt-6 text-teal-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function Services() {
   return (
     <>
-    < div className="grid grid-cols-12 p-6 gap-x-3 bg-white">
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl p-4 text-white  col-span-6">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Exquisite Presentation</h2>
-          <p className="mt-6 text-teal-600">Our gifts aren't just products, they are elegantly
-                    packaged to leave a lasting impression, show-casing
-                    your attention to detail and appreciation for the
-                    recipient
-          </p>
-          </div>
-          </div>
-          </div>
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl p-4 text-white  col-span-6">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Unmatched Customer Support</h2>
-          <p className="mt-6 text-teal-600">Our responsive customer support team is always at
-                your service ensuring a smooth and stress-free
-                gifting experience
-          </p>
-          </div>
-          </div>
-          </div>
-          
-          
-          
-    </div>
-    < div className="grid grid-cols-12 p-6 gap-x-3 bg-white">
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl p-4 text-white  col-span-6">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Sustainability Commitment</h2>
-          <p className="mt-6 text-teal-600">We're dedicated to eco-conscious gifting. Our
-        catalog features a range of eco - friendly
-        products , aligning your brandwith
-        sustainability goals
-          </p>
-          </div>
-          </div>
-          </div>
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl p-4 text-white  col-span-6 ">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Cost-Efficiency Beyond MRP</h2>
-          <p className="mt-6 text-teal-600">we prioritize affordability.Our exclusive
-partnerships enable us to offer you corporate gifts
-at prices lower than their MRP, ensuring exceptional
-value for your investment
-          </p>
-          </div>
-          </div>
-          </div>
-          
-          
-          
-    </div>
-    < div className="grid grid-cols-12 p-6 gap-x-3 bg-white">
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl col-p-4  text-white  col-span-6">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Effortless Corporate Gifting</h2>
-          <p className="mt-6 text-teal-600">Simplify your gifting process with our themed gift
-sets and expert curation, making it effortless for
-you to select the perfect gifts for your clients,
-employees, and partners.
-          </p>
-          </div>
-          </div>
-          </div>
-          <div className="min-h-[100px] rounded  bg-yellow-100 w-full shadow-xl col-p-4 text-white  col-span-6">
-          <div className="p-4 text-center flex flex-col justify-around h-full">
-          <div className="flex flex-col gap-y-2">
-          <h2 className="text-l text-teal-900 font-bold md:text-xl">Diverse Curated Selection</h2>
-          <p className="mt-6 text-teal-600">We excel in meticulously curating top-tie brands
-across a wide spectrum of categories ensuring
-that our corporate gifts are a fusion of
-innovation, quality, and purpose
-          </p>
-          </div>
-          </div>
-          </div>
-          
-          
-          
-    </div>
-   </> 
+      <div className="grid grid-cols-12 gap-x-4 gap-y-5 p-6 bg-white">
+        {serviceCardDetails.map((item: IServiceData) => (
+          <ServiceCard
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            id={item.id}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
-export default Services
+export default Services;
