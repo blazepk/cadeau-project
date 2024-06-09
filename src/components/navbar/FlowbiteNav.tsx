923fcbbdafe8789fa9cb2becd393ae5dd0d3474d
blazepk <pratyushkk880@gmail.com>
2024-06-09 18:42:35 +0530

    few fixes few remaining

--- src/components/navbar/FlowbiteNav.tsx
+++ src/components/navbar/FlowbiteNav.tsx
@@ -1,102 +1,105 @@
-import Dlogo from '../../assets/AbstractWaves.png';
-import { Link, NavLink } from 'react-router-dom'
+import Dlogo from "../../assets/AbstractWaves.png";
+import { Link, NavLink } from "react-router-dom";
 
-import { BsSearch } from "react-icons/bs"
-import { AiOutlineUser } from "react-icons/ai";
-import { RiShoppingBag3Line } from "react-icons/ri"
+// import { BsSearch } from "react-icons/bs"
+// import { AiOutlineUser } from "react-icons/ai";
+// import { RiShoppingBag3Line } from "react-icons/ri"
 
 function NavComponent() {
- 
   return (
     <nav className="bg-yellow-100 border-gray-200 dark:bg-gray-900">
-      < div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to ="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <img src={Dlogo} className="h-8" alt="Navbar Logo" />
-          
-          
+
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-yellow-600">
             ParkhaVentures
           </span>
         </Link>
-        < div className="flex items-center lg:order-2">
-                        
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-         
-         
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+        <div className="flex items-center lg:order-2">
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
 
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 text-white ${
+                      isActive ? "text-orange-700" : "text-gray-700"
+                    } bg-yellow-700 rounded md:bg-transparent md:text-yellow-700 md:p-0 dark:text-white md:dark:text-yellow-500"
+                aria-current="page"`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/About"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 text-gray-900 rounded ${
+                      isActive ? "text-orange-700" : "text-gray-700"
+                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/Services"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 text-gray-900 rounded ${
+                      isActive ? "text-orange-700" : "text-gray-700"
+                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`
+                  }
+                >
+                  Services
+                </NavLink>
+              </li>
 
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-           
-            <li>
-              <NavLink to ="/"
-                className={({isActive}) => 
-                  `block py-2 px-3 text-white ${isActive ? "text-orange-700" : "text-gray-700"} bg-yellow-700 rounded md:bg-transparent md:text-yellow-700 md:p-0 dark:text-white md:dark:text-yellow-500"
-                aria-current="page"`} >
-                
-              
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to ="/About"
-                
-                className={({isActive}) => `block py-2 px-3 text-gray-900 rounded ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to  ="/Services"
-                className={({isActive}) => `block py-2 px-3 text-gray-900 rounded ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-              >
-                Services
-              </NavLink>
-            </li>
-            
-            <li>
-              <NavLink to ="/Contact"
-                className= {({isActive})=> `block py-2 px-3 text-gray-900 rounded ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-              >
-                Contact
-              </NavLink>
-            </li>
+              <li>
+                <NavLink
+                  to="/Contact"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 text-gray-900 rounded ${
+                      isActive ? "text-orange-700" : "text-gray-700"
+                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-700 md:p-0 dark:text-white md:dark:hover:text-yellow-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
             </ul>
-          
-          
-         </div>
+          </div>
         </div>
-         
       </div>
-              
-
-            
-           
-          
     </nav>
   );
 }
